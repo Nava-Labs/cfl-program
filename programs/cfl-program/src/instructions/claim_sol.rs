fafe100d1c5b4e78092c290aa82a5b0c3786c4b6
d1ceeth <erwinphanglius10@gmail.com>
2025-03-08 14:30:08 +0700

    fix: add claim constraint and checks

--- programs/cfl-program/src/instructions/claim_sol.rs
+++ programs/cfl-program/src/instructions/claim_sol.rs
@@ -8,6 +8,19 @@ pub fn claim_sol(ctx: Context<ClaimSol>, _match_id: u64) -> Result<()> {
     let user = &mut ctx.accounts.user;
     let rent = &mut ctx.accounts.rent;
 
+    let squad_winner_account = &ctx.accounts.squad_winner;
+    let mut squad_winner_account_data: &[u8] = &squad_winner_account.data.borrow();
+    let squad_winner_data: Squad =
+        AccountDeserialize::try_deserialize(&mut squad_winner_account_data)?;
+
+    if squad_winner_data.owner != user.key() {
+        return err!(CustomError::InvalidSquadOwner);
+    }
+
+    if squad_winner_account.key() != match_account.winner {
+        return err!(CustomError::InvalidSquadWinner);
+    }
+
     let account_rent = Rent::minimum_balance(rent, Match::ACCOUNT_SIZE);
 
     let sol_to_withdraw = match_account.get_lamports() - account_rent;
@@ -27,6 +40,10 @@ pub struct ClaimSol<'info> {
         bump
     )]
     pub match_account: Account<'info, Match>,
+
+    /// CHECK:
+    #[account(mut, owner =  crate::ID)]
+    pub squad_winner: AccountInfo<'info>,
 
     #[account(mut)]
     pub user: Signer<'info>,
