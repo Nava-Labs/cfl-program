9e7fb173186bc3e785e211474f865e62a4920ac2
d1ceeth <erwinphanglius10@gmail.com>
2025-03-08 14:28:51 +0700

    fix: custom errors

--- programs/cfl-program/src/errors.rs
+++ programs/cfl-program/src/errors.rs
@@ -2,14 +2,35 @@ use anchor_lang::prelude::*;
 
 #[error_code]
 pub enum CustomError {
-    #[msg("Invalid squad")]
-    InvalidSquad,
+    #[msg("Invalid squad index")]
+    InvalidSquadIndex,
+
+    #[msg("Invalid price feed length")]
+    InvalidPriceFeedLength,
+
+    #[msg("Invalid weight percentage")]
+    InvalidWeightPercentageLength,
+
+    #[msg("Invalid match id")]
+    InvalidMatchId,
+
+    #[msg("Invalid timestamp")]
+    InvalidTimestamp,
+
+    #[msg("Invalid duration")]
+    InvalidDuration,
+
+    #[msg("Invalid account")]
+    InvalidAccount,
+
+    #[msg("Invalid squad owner")]
+    InvalidSquadOwner,
+
+    #[msg("Invalid squad winner")]
+    InvalidSquadWinner,
 
     #[msg("Match expired")]
     MatchExpired,
-
-    #[msg("Sol Claimed!")]
-    SolClaimed,
 
     #[msg("Match Started")]
     MatchStarted,
